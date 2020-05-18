0c56451d5d75cba633be60104ea9123a960744da
karoliks <karoline.k.sabo@gmail.com>
2020-05-18 08:09:05 +0200

    Add impassable objects

--- src/features/world/index.js
+++ src/features/world/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import Player from "../player";
 import Map from "../map";
 
+import { tiles } from "../../data/maps/1";
+import store from "../../config/store";
+
 function World(props) {
+  store.dispatch({
+    type: "ADD_TILES",
+    payload: {
+      tiles: tiles,
+    },
+  });
   return (
     <div
       style={{
