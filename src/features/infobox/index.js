dd0ca93a4f0c24cbc2c3fc0fb6d85e386dc3b7c5
karoliks <karoline.k.sabo@gmail.com>
2020-06-07 10:49:08 +0200

    Clean code

--- src/features/infobox/index.js
+++ src/features/infobox/index.js
@@ -1,38 +1,9 @@
-import React, { useState, useEffect } from "react";
-import store from "../../config/store";
-
+import React from "react";
 import { connect } from "react-redux";
 import "./styles.css";
 
-// do not think this part is ideal, but it works:)
-
 function InfoBox({ show, type }) {
   const showHideClassName = show ? " display-block" : " display-none";
-
-  // connects key-pressing to actions
-  //   useEffect(() => {
-  //     function handleKeyDown(event) {
-  //       if (
-  //         event.code === "Enter" ||
-  //         event.code === "NumpadEnter" ||
-  //         event.code === "Space"
-  //       ) {
-  //         store.dispatch({
-  //           type: "HIDE_INFOBOX",
-  //         });
-  //       }
-  //     }
-  //     // only interact with the modal if it is showing
-  //     if (store.getState().modal.show) {
-  //       const listener = (event) => {
-  //         handleKeyDown(event);
-  //       };
-  //       document.addEventListener("keydown", listener);
-  //       return () => {
-  //         document.removeEventListener("keydown", listener);
-  //       };
-  //     }
-  //   });
 
   return (
     <div className={showHideClassName}>
