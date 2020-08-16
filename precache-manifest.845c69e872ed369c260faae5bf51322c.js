6812f404a6c027644d4c1a4f414be593c5454cac
karoliks <karoline.k.sabo@gmail.com>
2020-08-16 09:57:10 +0200

    Updates

---
+++ precache-manifest.845c69e872ed369c260faae5bf51322c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c6f2e8563215b4fc74ec239026f31a",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "b621b8d93cb4ac2f4c56",
+    "url": "/react-game/static/css/main.b1615eca.chunk.css"
+  },
+  {
+    "revision": "4b51830f26d764bc9c71",
+    "url": "/react-game/static/js/2.4e7cd50c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-game/static/js/2.4e7cd50c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b621b8d93cb4ac2f4c56",
+    "url": "/react-game/static/js/main.93066c20.chunk.js"
+  },
+  {
+    "revision": "a6d903b9dfdf47e21475",
+    "url": "/react-game/static/js/runtime-main.538ef4b3.js"
+  }
+]);
